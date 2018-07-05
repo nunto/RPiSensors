005812fec20da41ed77e6d607849784d11b8e62e
Dimitry Linkov <dimitry@ilinkov.com>
2018-07-05 11:49:44 -0400

    Added GUI styling and GUI entry data transfer

--- lib/SensorPiGUI.py
+++ lib/SensorPiGUI.py
@@ -14,7 +14,7 @@ class GraphicInterface:
         self.is_Ready = False
         self.mainframe = None
         self.root = None
-        
+#TO DO Name vqariables and menus to be more specific     
         
     def createwindow(self):
         self.root = Tk()
@@ -28,42 +28,58 @@ class GraphicInterface:
 
         self.root.title("SensorPi")
         self.root.geometry("1900x1300")
+        self.root.config(bg='#e6e6ff')
 
 
         self.mainframe = Frame(self.root)
         self.mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
         self.mainframe.columnconfigure(0, weight = 1)
         self.mainframe.rowconfigure(0, weight = 1)
-        self.mainframe.pack(pady = 100, padx = 10)
+        self.mainframe.pack(pady = 100, padx = 100)
+        self.mainframe.config(bg='#e6e6ff')
          
         
          
         # List with options
         choices = [ '0.5','1.0','1.5','2.0','2.5']
 
-        #setting up the various pop-up menus to make the choices available 
+        #setting up the various pop-up menus to make the choices available
+        Label(self.mainframe, text= "Pi Sensor Configuration", font='arial 24 bold italic underline', fg='black', bg='#e6e6ff').grid(row = 0, column = 2)
         popupMenu1 = OptionMenu(self.mainframe, self.tkvar1, *choices, command=self.setDHT)
-        Label(self.mainframe, text="Choose the frequency of checking the DHT sensor (every how many minutes)   ").grid(row = 1, column = 1)
+        Label(self.mainframe, text="Choose the frequency of checking the DHT sensor (every how many minutes)   ", font='arial 12', fg='#ff1a1a', bg='#e6e6ff').grid(row = 1, column = 1)
         popupMenu1.grid(row = 2, column = 1)
         popupMenu1["menu"].config(bg="WHITE")
+        popupMenu1.config(bg="WHITE")
+        popupMenu1.config(fg='blue')
+        popupMenu1["menu"].config(fg='BLUE')
         self.tkvar1.set('1.0') # set the default option
 
         popupMenu2 = OptionMenu(self.mainframe, self.tkvar2, *choices, command = self.setCurrent)
-        Label(self.mainframe, text="Choose the frequency of checking the current sensor (every how many minutes)   ").grid(row = 1, column = 2)
+        Label(self.mainframe, text="Choose the frequency of checking the current sensor (every how many minutes)   ", font='arial 12', fg='#ff1a1a', bg='#e6e6ff').grid(row = 1, column = 2)
         popupMenu2.grid(row = 2, column = 2)
         popupMenu2["menu"].config(bg="WHITE")
+        popupMenu2.config(bg="WHITE")
+        popupMenu2.config(fg='blue')
+        popupMenu2["menu"].config(fg='BLUE')
         self.tkvar2.set('1.0') # set the default option
 
         popupMenu3 = OptionMenu(self.mainframe, self.tkvar3, *choices, command = self.setRPM)
-        Label(self.mainframe, text="Choose the frequency of checking the RPM sensor (every how many minutes)   ").grid(row = 1, column = 3)
+        Label(self.mainframe, text="Choose the frequency of checking the RPM sensor (every how many minutes)   ", font='arial 12', fg='#ff1a1a', bg='#e6e6ff').grid(row = 1, column = 3)
         popupMenu3.grid(row = 2, column = 3)
         popupMenu3["menu"].config(bg="WHITE")
+        popupMenu3.config(bg="WHITE")
+        popupMenu3.config(fg='blue')
+        popupMenu3["menu"].config(fg='BLUE')
         self.tkvar3.set('1.0') # set the default option
 
 
-        SubmitButton = Button(self.mainframe, text = "Submit", command= lambda: self.buttonClick())
+        SubmitButton = Button(self.mainframe, text = "Submit", font='arial 12', bg='#80d4ff', fg='#9966ff', command= lambda: self.buttonClick())
         SubmitButton.grid(row = 3, column = 2)
+    
+        SubmitButton.bind("<Enter>", lambda event, h=SubmitButton: h.configure(bg="#ccffcc"))
+        SubmitButton.bind("<Leave>", lambda event, h=SubmitButton: h.configure(bg='#80d4ff'))
 
+        
         #self.tkvar1.trace('w', self.change_dropdown)
         #self.tkvar2.trace('w', self.change_dropdown)
         #self.tkvar3.trace('w', self.change_dropdown)
@@ -84,15 +100,13 @@ class GraphicInterface:
         self.RPMInterval = value
     
     def dismiss(self):
-        self.mainframe.destroy()
+        #self.mainframe.destroy()
         self.root.destroy()
     
     def buttonClick(self):
         self.is_Ready = True
         self.dismiss()
 
-gui = GraphicInterface(1,1,1)
-gui.createwindow()
 
 
 
