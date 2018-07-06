e3bd9f6f53e7cacb851686f9640dfad5fc06d58d
Dimitry Linkov <dimitry@ilinkov.com>
2018-07-06 11:01:04 -0400

    Added third thread to the main test file

--- lib/SensorTestWithThreads.py
+++ lib/SensorTestWithThreads.py
@@ -10,8 +10,28 @@ import sys
 import socket
 #import json
 #import urllib2
+#Connection variable initialized and lock set up
 ConnectionLock= Semaphore(1)
 is_connected = False
+
+#Thread Classes setup 
+class ConnectionCheckThread (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        connectiontest()
+
+class SQLSendThread (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        OnlineDump()
+        
+class LocalDataDump (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        OfflineDump()
 
 def connectiontest():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,31 +43,41 @@ def connectiontest():
         is_connected = False
     ConnectionLock.release()
 
-class ConnectionCheckThread (threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-    def run(self):
-        connectiontest()
 
-class SQLSendThread (threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-    def run(self):
-        mainfunction()
+def ReadingData():
+    # Setting up the Raspberry Pi
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.cleanup()
 
-#methods to prpoerly retrieve values put into the GUI
-DHTInterval = 0
-def getDHTInterval():
-    return float(graphic.DHTInterval)
+        
 
-def getCurrentInterval():
-    return float(graphic.CurrentInterval)
+    # Setting up the Temp & Humidity sensor
+    sensor = DHT11.DHT11(pin=14)
+    #sets reading to be the sensor data
+    reading = sensor.read()
 
-def getRPMInterval():
-    return float(graphic.RPMInterval)
+#Online sending methods
+
+def OnlineDump():
+    #executing everything indefinitely in a loop
+    while True:
+        ConnectionLock.acquire()
+        if(is_connected):
+            timedsendOnline()
+            ConnectionLock.release()
+        else:
+            ConnectionLock.release()
+
+def timedsendOnline():
+    global current_time
+    #checks if the right number of seconds have passed based on the GUI 
+    if(current_time + getDHTInterval() <= int(time.mktime(dt.datetime.now().timetuple()))):
+        SQLsend()
+        current_time = int(time.mktime(dt.datetime.now().timetuple()))
 
 def SQLsend():
-    reading = sensor.read()
+    
     #checks if there is data still in the reading
     if reading.is_valid():
 
@@ -66,28 +96,49 @@ def SQLsend():
         dataInsert = "INSERT INTO TempHumidity(Timestamp, Temperature, Humidity) values (%d, %d, %d)" % (current_time, reading.temperature, reading.humidity)
         cursor.execute(dataInsert)
         cnxn.commit()
-    
-def timedsend():
+
+#Offline data sending methods
+
+def OfflineDump():
+    #executing everything indefinitely in a loop
+    while True:
+        ConnectionLock.acquire()
+        if(not is_connected):
+            timedsendOffline()
+            ConnectionLock.release()
+        else:
+            ConnectionLock.release()
+
+def timedsendOffline():
     global current_time
     #checks if the right number of seconds have passed based on the GUI 
     if(current_time + getDHTInterval() <= int(time.mktime(dt.datetime.now().timetuple()))):
-        SQLsend()
+        LocalFileSend()
         current_time = int(time.mktime(dt.datetime.now().timetuple()))
 
-def mainfunction():
-    # Setting up the Raspberry Pi
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.cleanup()
+def LocalFileSend():
+    if reading.is_valid():
+        a= [current_time, reading.temperature, reading.temperature]
+        with open('localdatadump.pickle', 'wb') as handle:
+            pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            
 
-        
+#methods to prpoerly retrieve values put into the GUI
+DHTInterval = 0
+def getDHTInterval():
+    return float(graphic.DHTInterval)
 
-    # Setting up the Temp & Humidity sensor
-    sensor = DHT11.DHT11(pin=14)
+def getCurrentInterval():
+    return float(graphic.CurrentInterval)
 
-    # Reads the current sensor data
+def getRPMInterval():
+    return float(graphic.RPMInterval)
 
 
+#GUI Popup method called when program is started
+
+def GUIPopup():
+    
     graphic = gui.GraphicInterface()
     graphic.createwindow()
     #once the GUI submit button has been pressed
@@ -97,15 +148,7 @@ def mainfunction():
     # Setting current time
     current_time = int(time.mktime(dt.datetime.now().timetuple()))
 
-
-    #executing everything indefinitely in a loop
-    while True:
-        ConnectionLock.acquire()
-        if(is_connected):
-            timedsend()
-            ConnectionLock.release()
-        else:
-            ConnectionLock.release()
+    
 """
 else:
     if (reading.error_code == 1):
@@ -113,12 +156,17 @@ else:
     elif (reading.error_code == 2):
         print("Error %d: CRC" % reading.error_code)
 """
+GUIPopup()
+ReadingData()
 
 ConnectionThread = ConnectionCheckThread()
-SQLSend = SQLSendThread()
+SQLSending = SQLSendThread()
+LocalDataDumping = LocalDataDump()
 
 ConnectionThread.start()
-SQLSend.start()
-ConnectionThread.join()
-SQLSend.join()
+SQLSending.start()
+LocalDataDumping.start()
 
+ConnectionThread.join()
+SQLSending.join()
+LocalDataDumping.join()
